e780492a012f709c52796c9ca048c02267a97b38
Hasibur14 <hasib141415@gmail.com>
2025-01-13 11:51:03 +0600

    [UPDATE] tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from 'daisyui';
+
 export default {
   content: [
     "./index.html",
@@ -8,8 +10,6 @@ export default {
     extend: {},
   },
   plugins: [
-    // eslint-disable-next-line no-undef
-    require('daisyui'),
+    daisyui, // Use daisyui plugin with import
   ],
-}
-
+};
