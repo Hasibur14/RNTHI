9eb2f7a8052ae6e6c2c184af221ea07eaf4d4122
Hasibur14 <hasib141415@gmail.com>
2025-01-13 16:58:28 +0600

    [ADD] auto slider

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,94 +1,138 @@
-import React, { useState } from 'react';
-import slider1 from "../../../src/assets/hero/slider10.png";
-import slider2 from "../../../src/assets/hero/slider.jpg";
+import React, { useState } from "react";
+import slider1 from "../../../src/assets/hero/slider.jpg";
 import slider3 from "../../../src/assets/hero/slider5.jpg";
-import Sidebar from '../Sidebar/Sidebar';
+import Sidebar from "../Sidebar/Sidebar";
+import { useKeenSlider } from "keen-slider/react";
+import "keen-slider/keen-slider.min.css";
+import { FiArrowDownRight, FiArrowUpRight } from "react-icons/fi";
 
 const Hero = () => {
-    // Sample JSON data
     const sliderData = [
         {
             id: 1,
             image: slider1,
-            title: 'Welcome To RNTHI Institute',
-            description: 'Description for Slide 1',
+            title: "Welcome To RNTHI Institute",
+            description: "Description for Slide 1",
         },
         {
             id: 2,
-            image: slider2,
-            title: 'Slide 2',
-            description: 'Description for Slide 2',
+            image: slider3,
+            title: "Slide 2",
+            description: "Description for Slide 2",
         },
         {
             id: 3,
             image: slider3,
-            title: 'Slide 3',
-            description: 'Description for Slide 3',
+            title: "Slide 3",
+            description: "Description for Slide 3",
         },
     ];
 
+    // Slider with auto-play
+    const [sliderRef, instanceRef] = useKeenSlider(
+        {
+            loop: true,
+            slideChanged(slider) {
+                setCurrentIndex(slider.track.details.rel);
+            },
+        },
+        [
+            (slider) => {
+                let timeout;
+                let mouseOver = false;
+
+                function clearNextTimeout() {
+                    clearTimeout(timeout);
+                }
+
+                function nextTimeout() {
+                    clearTimeout(timeout);
+                    if (mouseOver) return;
+                    timeout = setTimeout(() => {
+                        slider.next();
+                    }, 5000);
+                }
+
+                slider.on("created", () => {
+                    slider.container.addEventListener("mouseover", () => {
+                        mouseOver = true;
+                        clearNextTimeout();
+                    });
+                    slider.container.addEventListener("mouseout", () => {
+                        mouseOver = false;
+                        nextTimeout();
+                    });
+                    nextTimeout();
+                });
+
+                slider.on("dragStarted", clearNextTimeout);
+                slider.on("animationEnded", nextTimeout);
+                slider.on("updated", nextTimeout);
+            },
+        ]
+    );
+
+    // Active slide index
     const [currentIndex, setCurrentIndex] = useState(0);
 
+    // Slider navigation handlers
     const handlePrevious = () => {
-        setCurrentIndex((prevIndex) =>
-            prevIndex === 0 ? sliderData.length - 1 : prevIndex - 1
-        );
+        instanceRef.current?.prev();
     };
 
     const handleNext = () => {
-        setCurrentIndex((prevIndex) =>
-            prevIndex === sliderData.length - 1 ? 0 : prevIndex + 1
-        );
+        instanceRef.current?.next();
     };
 
     return (
-        <div className="absolute w-full mx-auto bg-black bg-opacity-20 bg-blend-overlay">
-            <div className="relative ">
-                {/* Slider Image with Text overlay */}
-
-                {sliderData.map((item, index) => (
+        <div className="absolute w-full mx-auto">
+              {/* Sidebar */}
+              <div className="relative top-0 border-b z-[9999]">
+                <Sidebar />
+            </div>
+            {/* Slider Section */}
+            <div ref={sliderRef} className="relative keen-slider font-rajdhani h-auto">
+                {sliderData.map((slide, index) => (
                     <div
-                        key={item.id}
-                        className={`absolute inset-0 transition-transform duration-500 ${index === currentIndex ? 'opacity-100' : 'opacity-0'
-                            }`}
+                        key={index}
+                        className={`keen-slider__slide number-slide relative inset-0`}
                     >
                         <img
-                            src={item.image}
-                            alt={item.title}
-                            className="absolute w-full h-[900px] object-cover"
+                            className="w-full h-[50vh] sm:h-[60vh] md:h-[70vh] object-cover"
+                            src={slide.image}
+                            alt={slide.title}
                         />
-                        <div className="absolute inset-0 mt-96 flex items-center justify-center bg-black bg-opacity-50">
-                            <div className="text-center p-10 text-white">
-                                <h2 className="text-7xl font-bold mb-4 font-cinzel uppercase">{item.title}</h2>
-                                <p className="text-lg">{item.description}</p>
-                            </div>
+                        <div className="absolute inset-0 bg-black bg-opacity-50 flex flex-col text-center justify-center space-y-4 px-6 sm:px-8 lg:px-20">
+                            <h2 className="font-rajdhani text-4xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl font-bold text-white">
+                                {slide.title}
+                            </h2>
+                            <p className="font-heebo text-md sm:text-lg md:text-2xl text-gray-300">
+                                {slide.description}
+                            </p>
                         </div>
                     </div>
                 ))}
             </div>
 
-            <div className='relative top-0 border-b z-50'>
-                <Sidebar />
-            </div>
+          
 
-            <div className='absolute text-center mt-10 justify-center items-center flex space-x-4 w-full'>
-                {/* Navigation Controls */}
+            {/* Navigation Controls */}
+            <div className="absolute text-center mt-10 justify-center items-center flex space-x-4 w-full">
                 <button
                     onClick={handlePrevious}
                     className="bg-gray-800 text-white px-4 py-2 rounded-full hover:bg-gray-700"
+                    aria-label="Previous Slide"
                 >
                     Previous
                 </button>
-                {/* Slider Indicators */}
                 <div className="flex justify-center h-5 space-x-4">
                     {sliderData.map((_, index) => (
                         <button
                             key={index}
-                            onClick={() => setCurrentIndex(index)}
-                            className={`text-xl font-bold border-r border-gray-400 ${currentIndex === index
-                                ? 'text-primary-light '
-                                : 'text-white'
+                            onClick={() => instanceRef.current?.moveToIdx(index)}
+                            className={`text-xl font-bold border-r border-gray-400 px-2 ${currentIndex === index ? "text-primary-light" : "text-white"
                                 }`}
+                            aria-label={`Go to slide ${index + 1}`}
                         >
                             0{index + 1}
                         </button>
@@ -97,6 +141,7 @@ const Hero = () => {
                 <button
                     onClick={handleNext}
                     className="bg-gray-800 text-white px-4 py-2 rounded-full hover:bg-gray-700"
+                    aria-label="Next Slide"
                 >
                     Next
                 </button>
