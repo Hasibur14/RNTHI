6e15a1a4b043f63e66e111351650afa1557c8c45
Hasibur14 <hasib141415@gmail.com>
2025-01-13 12:57:31 +0600

    [ADD] sidebar

--- src/Layout/Root.jsx
+++ src/Layout/Root.jsx
@@ -1,11 +1,14 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../components/Shared/Footer/Footer";
 import Navbar from "../components/Shared/Navbar/Navbar";
+import Sidebar from "../components/Sidebar/Sidebar";
 
 const Root = () => {
     return (
-        <div className="font-lato bg-gradient-to-r from-blue-500 to-purple-600 text-white">
+        <div className="">
             <div className="h-16 shadow-lg">
+                <Sidebar />
+                <hr />
                 <Navbar />
             </div>
             <div className="min-h-[calc(100vh-300px)] flex items-center justify-center">
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,11 +1,56 @@
 import React from 'react'
+import { FaFacebookF, FaLinkedinIn } from 'react-icons/fa';
+import { FaXTwitter } from 'react-icons/fa6';
+import { IoLogoYoutube } from 'react-icons/io';
+import { IoMenuOutline } from 'react-icons/io5';
+import { Link } from 'react-router-dom';
 
 const Sidebar = () => {
     return (
-        <div>
-            
+        <div className='container mx-auto max-w-7xl flex py-2'>
+            {/* Sidebar menu */}
+            <div className="drawer">
+                <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+                <div className="drawer-content">
+                    {/* Page content here */}
+                    <label htmlFor="my-drawer" className="drawer-button">
+                        <IoMenuOutline className='text-4xl' />
+                    </label>
+                </div>
+                <div className="drawer-side mt">
+                    <label htmlFor="my-drawer" aria-label="close sidebar" className="drawer-overlay"></label>
+                    <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4 pt-16">
+                        {/* Sidebar content here */}
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/about">About</Link>
+                        </li>
+                        <li>
+                            <Link to="/about">Comtact</Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            {/* Social Media */}
+            <div className="flex space-x-4">
+                <a href="https://facebook.com" target="_blank" rel="noopener noreferrer" aria-label="Follow us on Facebook">
+                    <FaFacebookF className="text-2xl p-1 rounded bg-primary-light text-white hover:bg-white hover:text-pribg-primary-light transition duration-500" />
+                </a>
+                <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer" aria-label="Follow us on LinkedIn">
+                    <FaLinkedinIn className="text-2xl p-1 rounded bg-primary-light text-white hover:bg-white hover:text-pribg-primary-light transition duration-500" />
+                </a>
+                <a href="https://twitter.com" target="_blank" rel="noopener noreferrer" aria-label="Follow us on Twitter">
+                    <FaXTwitter className="text-2xl p-1 rounded bg-primary-light text-white hover:bg-white hover:text-pribg-primary-light transition duration-500" />
+                </a>
+                <a href="mailto:info@example.com" target="_blank" rel="noopener noreferrer" aria-label="Email us">
+                    <IoLogoYoutube className="text-2xl p-1 rounded bg-primary-light text-white hover:bg-white hover:text-pribg-primary-light transition duration-500" />
+                </a>
+
+            </div>
         </div>
     )
 }
 
-export default Sidebar ;
+export default Sidebar;
